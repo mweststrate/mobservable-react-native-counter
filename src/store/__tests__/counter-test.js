76dba47625d0062e4f7fc652e1a4ba26ee48596f
Michel Weststrate <mweststrate@gmail.com>
2016-02-22 22:30:21 +0100

    JEST tests in progress

--- src/store/__tests__/counter-test.js
+++ src/store/__tests__/counter-test.js
@@ -2,10 +2,36 @@
 
 jest.autoMockOff();
 
+var React =  require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+
+var counterStore = require('../counter');
+var CounterUI = require('../../CounterUI');
+
 describe('ErrorAlert', () => {
   it('should be fine', () => {
     expect(true).toEqual(true);
   });
+});
 
+describe('it should could', () => {
+
+  it('changes', () => {
+	console.log('hoi');
+	var timerComponent = TestUtils.renderIntoDocument(
+		React.createElement(CounterUI, { store: counterStore })
+	);
+
+    var timerNode = ReactDOM.findDOMNode(timerComponent);
+
+    expect(timerNode.textContent).toEqual('Off');
+	console.log('boe');
+
+	// setTimeout(() => {
+	// 	TestUtils.findRenderedDOMComponentWithTag(timerComponent, 'input')
+    // 	expect(timerNode.textContent).toEqual('On');
+	// }, 2000);
+  });
 
 });
